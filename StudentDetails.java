dc68e68e7d81deb0e00500cea1213c175df3886c
deepti <deeeptiyatagiri@gmail.com>
2025-04-29 11:47:08 +0530

    save

--- StudentDetails.java
+++ StudentDetails.java
@@ -12,10 +12,18 @@ public class StudentDetails {
         System.out.print("Enter student ID: ");
         int id = scanner.nextInt();
 
+        System.out.println("Enter student phone number:");
+        String number =  scanner.nextLine();
+
+        System.out.println("Enter student address");
+        String address = scanner.nextLine();
+
         // Display the entered details
         System.out.println("\nStudent Details:");
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
+        System.out.println("phone Number" +number);
+        System.out.println("address:" +address)
 
         scanner.close();
     }
